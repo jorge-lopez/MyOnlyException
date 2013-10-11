97b4f35fefae8c4c6c649cb9e3e08b378e89e6c9
jorge-lopez <jorge.lopez.18820@gmail.com>
2013-10-10 17:43:39 -0700

    First Commit
    First Commit

---
+++ MyException/MyException.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyException
+{
+    class MyException : Exception
+    {
+        private string _Message;
+
+        public MyException()
+            : base() 
+        {
+            _Message = "Exception handled";
+        }
+        public MyException(string message)
+            : base (message)
+        {
+            _Message = message;
+            
+        }
+ 
+        public override string Message
+        {
+           
+            get
+            {
+                return _Message;
+            }
+
+        }
+
+    }
+}
---
+++ MyException/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyException
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            //How hayley came up with a song
+
+            while (true)
+            {
+                try
+                {
+                    Console.Write("You are ");
+                    throw new MyException("The only exception");
+                }
+
+                catch (MyException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
